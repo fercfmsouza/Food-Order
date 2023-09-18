78e33ed63215f00c747260f8eb7b8da054b1661c
Fernanda Souza <fecafumee@gmail.com>
2023-09-18 15:43:10 +0200

    configure react app for deployment to github pages

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,48 @@
+import { useContext, useEffect, useState } from 'react';
+import classes from './HeaderCartButton.module.css';
+import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
+
+const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+  const cartCtx = useContext(CartContext);
+
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ''
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    //use the setTimout to remove the class after 300ms
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    //always a best practice to clean a setTimout
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
+  return (
+    <button className={btnClasses} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
---
+++ src/store/cart-context.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+//initialize this context with some inital context
+const CartContext = React.createContext({
+  items: [],
+  totalAmount: 0,
+  addItem: (item) => {},
+  removeItem: (id) => {},
+});
+
+export default CartContext;
